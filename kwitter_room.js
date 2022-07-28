c9768e16510c672a2b74e9e6f8d79327eb800464
tanjiro <92766559+tanjiroTnT@users.noreply.github.com>
2022-07-28 10:46:57 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkPbR4efBe87bLZnAAxalk27G6ejyDRhQ",
+    authDomain: "redsocial-d91e8.firebaseapp.com",
+    databaseURL: "https://redsocial-d91e8-default-rtdb.firebaseio.com",
+    projectId: "redsocial-d91e8",
+    storageBucket: "redsocial-d91e8.appspot.com",
+    messagingSenderId: "131944476687",
+    appId: "1:131944476687:web:bac3da7787a5c6ceba737a"
+  };
+  
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
